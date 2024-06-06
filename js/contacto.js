6ae3a9d6180d964fe730107e3d049585e8e18e4c
Arely <arely_apariciosoberanes@gmail.com>
2024-06-06 13:34:49 -0600

    Agregué la página de pago

--- js/contacto.js
+++ js/contacto.js
@@ -91,3 +91,25 @@ btnEnviar.addEventListener("click", function(event){
     console.log(isValid);
     console.log(txtEmail.value);
 });
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_61pzqvk';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
