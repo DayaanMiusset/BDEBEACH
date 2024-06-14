9f14b7fccab740ca7209c7e2fb8fb6557235634d
Asael <pianochess7@gmail.com>
2024-06-13 23:19:27 -0600

    Se creó el código JS para validar campos de formulario_creación. Se creo el formato JSON y se guardó en localStorage

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,7 +1,7 @@
 
 
 function addItem(item){
-  let tonos = item.color;
+  /* let tonos = item.color;
   console.log(tonos);
   let coloresHTML="";
 
@@ -9,22 +9,23 @@ function addItem(item){
     coloresHTML += `
     <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel1" value="" aria-label="..."  style ="background-color: ${color}">
     `
-  });
+  }); */
  
   const itemsContainer = document.getElementById("list-items");
-  itemsContainer.insertAdjacentHTML ("beforeend", `<div class="col-md-4">
+  itemsContainer.insertAdjacentHTML ("beforeend", 
+    `<div class="col-md-4" id="id${item.id}">
     <div class="card mb-4 box-shadow">
       <img class="card-img-top" data-src="#" alt="Algo lindo" src="${item.image}" data-holder-rendered="true">
     <!-- <img class="card-img-top" data-src="" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22469%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20469%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18feaf2c5a3%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A23pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18feaf2c5a3%22%3E%3Crect%20width%3D%22469%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22156.99166870117188%22%20y%3D%22122.62000007629395%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">-->
       <div class="card-body">
-        <p class="card-text"><strong>${item.title}</strong></p>
+        <p class="card-text">ID:${item.id} <br><strong>${item.title}</strong></p>
          <small class="card-text">${item.description}</small>
           <br><br>
-        <div>
-        ${coloresHTML}
+        <div class="colores">
+
         </div>
           <div class="d-flex ms-auto">
-           <small class="price" >$ ${item.price}</small>
+           <small class="price" >$${item.price} MXN</small>
            </div>
          
           <br><br>
@@ -44,7 +45,7 @@ function addItem(item){
     {
         "id": 1,
         "title": "Top Mykonos",
-        "price": "750.00 MXN",
+        "price": 750.00 ,
         "description": "Top estilo bikini ajuble con tiras en la espalda y cuello",
         "color": ["#000000","#FFD700"],
         "image": "./src/topmykonos1.jpg",
@@ -57,7 +58,7 @@ function addItem(item){
      {
         "id": 2,
         "title": "Top Bora Bora",
-        "price": "650.00 MXN",
+        "price": 650.00 ,
         "description": "Top con diseno ajustable en la frente ",
         "color": ["#FFCBDB", "#FF00FF","#77dde6","#FEF5E7","#F0B27A","#B695C0", "#FF3339","#006341","#FFD700"],
         "image": "./src/topborabora2.jpg",
@@ -70,7 +71,7 @@ function addItem(item){
     {
         "id": 3,
         "title": "Top Bahamas",
-        "price": "950.00 MXN",
+        "price": 950.00 ,
         "description": "Top con descote U en la espalda",
         "color": ["#000000","#FEF5E7","#454B1B","#A04000"],
         "image": "./src/topbahamas3.jpeg",
@@ -84,7 +85,7 @@ function addItem(item){
     {
         "id": 4,
         "title": "Falda Saint-Tropez",
-        "price": "1450.00 MXN",
+        "price": 1450.00 ,
         "description": "Falda estilo sirena midi",
         "color": ["#000000","#FFD700"],
         "image": "./src/faldasainttropez4.jpg",
@@ -97,7 +98,7 @@ function addItem(item){
      {
         "id": 5,
         "title": "Mini Falda Saint Tropez",
-        "price": "990.00 MXN",
+        "price": 990.00,
         "description": "Falda estilo sirena corta",
         "color": ["#A04000","#FEF5E7","#f9e79f","#efac87","#f0b27a "],
         "image": "./src/minifaldasainttropez5.jpg",
@@ -110,7 +111,7 @@ function addItem(item){
     {
         "id": 6,
         "title": "Falda Aruba",
-        "price": "1100.00 MXN",
+        "price": 1100.00 ,
         "description": "Mini falda con ajuste en la cintura",
         "color":  ["#000000","#FEF5E7","#454B1B","#A04000"],
         "image": "./src/faldaaruba6.jpg",
@@ -151,7 +152,7 @@ function addItem(item){
     {
         "id": 10,
         "title": "Set Rio",
-        "price": "1000.00 MXN",
+        "price": 1000.00 ,
         "description": "El set incluye 2 piezas: TOP Y BOTTOMS",
         "color": ["#FFCBDB", "#FF00FF","#77dde6","#FEF5E7","#F0B27A","#B695C0", "#FF3339","#006341","#FFD700"],
         "image": "./src/bikinisetrio10.jpg",
@@ -163,7 +164,7 @@ function addItem(item){
     {
         "id": 11,
         "title": "Set Bora Bora",
-        "price": "1200.00 MXN",
+        "price": 1200.00 ,
         "description": "El set incluye 2 piezas: TOP Y BOTTOMS",
         "color": ["#FFCBDB", "#FF00FF","#77dde6","#FEF5E7","#F0B27A","#B695C0", "#FF3339","#006341","#FFD700"],
         "image": "./src/bikinisetborabora11.jpg",
@@ -176,7 +177,7 @@ function addItem(item){
     {
         "id": 12,
         "title": "Set Bikini Hellow Kitty",
-        "price": "2590.00 MXN",
+        "price": 2590.00,
         "description": "El set incluye 3 piezas: TOP, BOTTOMS y PAREO",
         "color": ["#FFFFFF"],
         "image": "./src/bikinisethellokitty12.jpg",
@@ -188,7 +189,7 @@ function addItem(item){
         {
           "id": 8,
           "title": "Conjunto Bora Bora",
-          "price": "1640.00 MXN",
+          "price": 1640.00,
           "description": "El conjunto incluye 2 piezas: TOP Y FALDA MIDI con ajuste lateral",
           "color": ["#FFD700","#000000","#FFFFFF"],
           "image": "./src/conjuntoborabora8.jpg",
@@ -201,7 +202,16 @@ function addItem(item){
 
 items.forEach(item => addItem(item));
 
-/* items.forEach(item=> addColors(item));
-function addColors(articulo){
-console.log(articulo.color);
-} */
+async function nuevaCard(){
+  const datos = await JSON.parse(localStorage.getItem("datos"));
+  addItem(datos)
+}
+
+window.addEventListener("load", function(event){
+  event.preventDefault();
+
+  if(this.localStorage.getItem("datos")!=null){
+    nuevaCard()
+  }
+  
+});
---
+++ js/formulario_creacion.js
@@ -0,0 +1,143 @@
+let nombre = document.getElementById("nombre");
+let id = document.getElementById("id");
+let descripcion = document.getElementById("descripcion");
+let numeroDePiezas = document.getElementById("numeroDePiezas");
+let precio = document.getElementById("precio");
+let imagen = document.getElementById("imagen");
+let btnCrear = document.getElementById("btnCrear"); 
+let idRemove = document.getElementById("idRemove");
+let btnRemove = document.getElementById("btnRemove");
+let alertValidaciones = document.getElementById("alertValidaciones");
+let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+let btnImagen = document.getElementById("btnCrear"); 
+
+let datos = new Array();
+
+
+function validarNombre(){
+    if (nombre.value.length<3){
+        alertValidacionesTexto.innerHTML="El <strong>nombre</strong> no es correcto <br/>";
+        alertValidaciones.style.display="block";
+        alertValidaciones.style.border="solid red medium";
+        nombre.focus();
+        nombre.style.border="solid red medium";
+        return false;
+    }// if
+        return true;
+}//validarNombre
+
+function validarId (ID){
+    let patronId = new RegExp("^[0-9]{3}$");
+    if(ID.value.length==1){
+        ID.value="00" + ID.value;       
+    }else if(ID.value.length==2){
+        ID.value="0" + ID.value;
+    }
+    if(patronId.test(ID.value)){
+        return true;
+    }else{
+        alertValidacionesTexto.innerHTML +="El <strong>id</strong> no es válido. Debe contener 3 dígitos.<br/>";
+        alertValidaciones.style.display="block";
+        alertValidaciones.style.border="solid red medium";
+        ID.focus();
+        ID.style.border="solid red medium";
+        return false;
+    }//if
+}
+
+function validarDescripcion (){
+    let patronDesc = new RegExp("^.{10,60}$");
+
+    if(patronDesc.test(descripcion.value)){
+        return true;
+    }else{
+        if(descripcion.value.length<10){
+            alertValidacionesTexto.innerHTML +="Por favor, <strong>corrobora</strong> que la información sea correcta. <br/>";
+        }else{
+            alertValidacionesTexto.innerHTML +="Por favor, <strong>corrobora</strong> que la información no sobrepase los 60 caracteres. <br/>";
+        }
+        alertValidaciones.style.display="block";
+        alertValidaciones.style.border="solid red medium";
+        descripcion.focus();
+        descripcion.style.border="solid red medium";
+        return false;
+    }//if
+}//validarDescripcion
+
+function validarPrecio(){
+    let patronPrecio = new RegExp("^[0-9]{1,4}(\.[0-9]{1,2})?$");
+    if(patronPrecio.test(precio.value)){
+        return true;
+    }else{
+        alertValidacionesTexto.innerHTML +="El <strong>precio</strong> debe tener el siguiente formato: 1234.00 .<br/>";
+        alertValidaciones.style.display="block";
+        alertValidaciones.style.border="solid red medium";
+        precio.focus();
+        precio.style.border="solid red medium";
+        return false;
+    }//if
+}
+
+
+
+btnCrear.addEventListener("click", function(event){
+    event.preventDefault();
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    alertValidaciones.style.border="";
+    nombre.style.border="";
+    id.style.border="";
+    descripcion.style.border="";
+    precio.style.border="";
+
+    isValid = true;
+     console.log(validarNombre());
+     console.log(validarId(id));
+     console.log(validarDescripcion());
+     console.log(validarPrecio());
+
+     if (validarNombre() && validarId(id) && validarDescripcion() && validarPrecio()){
+        let row = `<tr>
+        <td>${nombre.value}</td>
+        <td>${descripcion.value}</td>
+        <td>${numeroDePiezas.value}</td>
+        <td>${precio.value}</td></tr>`;
+
+        let elemento = `{"id":${id.value},
+        "title":"${nombre.value}",
+        "description":"${descripcion.value}",
+        "numeroDePiezas":${numeroDePiezas.value},
+        "price":${precio.value},
+        "image":"./src/faldaaruba6_3.JPG"}`;
+        datos.push(JSON.parse(elemento));
+        localStorage.setItem("datos", JSON.stringify(datos));}
+});
+
+btnRemove.addEventListener("click", function(event){
+    let isValid = true;
+    event.preventDefault();
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    alertValidaciones.style.border="";
+    idRemove.style.border="";
+    
+    console.log(validarId(idRemove));
+    
+});
+
+
+/*Cloudinary*/
+/* var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dufshghoz', 
+    uploadPreset: 'bdebeach', 
+    }, 
+    (error, result) => {
+        if(!error && result && result.event==="success"){
+            console.log("hecho", result.info);
+        } 
+     });
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+    myWidget.open();
+    }, false); */
+        
